40fe30ef62d1a79747566945a4a965e2708984dc
UnknownBTW <64477446+UnknownBTW@users.noreply.github.com>
2026-02-18 19:48:14 +0000

    Bare bones

---
+++ os/target/debug/deps/os-679470580222cb84.d
@@ -0,0 +1,5 @@
+/workspaces/Rust_text_editor/os/target/debug/deps/os-679470580222cb84.d: src/main.rs
+
+/workspaces/Rust_text_editor/os/target/debug/deps/os-679470580222cb84: src/main.rs
+
+src/main.rs:
---
+++ os/target/debug/os.d
@@ -0,0 +1,1 @@
+/workspaces/Rust_text_editor/os/target/debug/os: /workspaces/Rust_text_editor/os/src/main.rs
